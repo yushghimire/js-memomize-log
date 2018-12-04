ebb1279f9803444b7afc718044c8ab04738af3a5
yushghimire <ayushghimire@lftechnology.com>
2018-12-04 11:10:52 +0545

    intial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+require('./index.js');
+
+function sqrt(args) {
+  return Math.sqrt(args);
+}
+
+var memomizeService = sqrt.memomize();
+
+console.time("Non memomized call");
+console.log(memomizeService(sqrt(9)));
+console.timeEnd("Non memomized call");
+
+console.time("Memomized call");
+console.log(memomizeService(sqrt(9)));
+console.timeEnd("Memomized call");
---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @description Know the difference of memomized and non-memomized call with time log.
+ * @author yushghimire
+ */
+
+module.exports = Function.prototype.memomize = function () {
+  var that = this;
+
+  return function() {
+    var args = Array.prototype.slice.call(arguments);
+    that.cache = that.cache || {};
+    
+    return that.cache[args] ? that.cache[args] : (that.cache[args] = that.apply(this, args));
+  }
+}
